1bea5b9e9ddb7dc12b8407ea621a7eed0d64f064
Yevgen9 <leon020584@gmail.com>
2024-01-26 14:51:27 +0200

    commit

--- js/task-03.js
+++ js/task-03.js
@@ -14,33 +14,13 @@ const images = [
 ];
 
 const galleryRef = document.querySelector(".gallery");
-const liOneRef = document.createElement("li");
-const liTwoRef = document.createElement("li");
-const liThreeRef = document.createElement("li");
 
-galleryRef.prepend(liOneRef);
-galleryRef.prepend(liTwoRef);
-galleryRef.prepend(liThreeRef);
-
-const imageOne = document.createElement("img");
-imageOne.src = images[0].url;
-imageOne.alt = images[0].alt;
-liOneRef.prepend(imageOne);
-
-const imageTwo = document.createElement("img");
-imageTwo.src = images[1].url;
-imageTwo.alt = images[1].alt;
-liTwoRef.prepend(imageTwo);
-
-const imageThree = document.createElement("img");
-// imageThree.src = images[2].url;
-imageThree.setAttribute(
-  "src",
-  "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-);
-
-// imageThree.alt = images[1].alt;
-imageThree.setAttribute("alt", "Group of Horses Running");
-liThreeRef.prepend(imageThree);
-
-console.log(galleryRef);
+images.map(({ url, alt }) => {
+  const createLi = document.createElement("li");
+  createLi.style.display = flex;
+  const createImage = document.createElement("img");
+  createImage.setAttribute("src", url);
+  createImage.setAttribute("alt", alt);
+  createLi.prepend(createImage);
+  galleryRef.prepend(createLi);
+});
